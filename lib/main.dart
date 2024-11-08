8978ca1e2e729858dae8b885620da2da4351c7ba
Alvin <alviura@outlook.com>
2024-11-08 17:22:17 +0300

    Created a basic widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,72 @@
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('im good to go'),
-        ),
-      ),
-    );
-  }
-}
+// ignore_for_file: prefer_const_constructors
 
 /* 
 
-WIDGETS -> Everything in flutter is a widget
+WIDGETS -> Everything in futter is a widget 
+The pre-built widgets in flutter that we can use are
+  Scaffold widget -> To
+  Text widget -> To create text
+  Button widget -> To create buttons
+  Row widget -> To deal with diff layouts in a row
+  Collumn widget -> "  "  "    "    "    "   "  column
+  image widget -> to deal with images
+
+Widgets have propeties that define how different element behave and look in the screen
+Widgets are classes
+
+S Y N T A X
+widgetName(
+  property: Widget()/value,
+  property2: value2,
+  property3: value3)
+
+  S T A T E L E S S    W I D G E T S
+  The state of the widget cannot change over time, the data can't change when it is initialized
+
+
+  S T A T E F U L      W I D G E T S
+  The state of widget can change over time
 
 */
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp( // the root widget of the application
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('This is a title'),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+      ),
+      
+      body: Center( // centers what is inside the widget
+        child: Text(
+          'hello ninjas',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+
+          )
+        ), // the child property is used if a widget happens to be a under that widget
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Text('Click'),
+        backgroundColor: Colors.redAccent,
+      ),
+    )
+  )); 
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
