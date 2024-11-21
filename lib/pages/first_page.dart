88ef4a8b311eba7e6fc45b75aab040eeccd95da1
Alviura <alviura@outlook.com>
2024-11-21 12:15:58 +0300

    Pages

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'second_page.dart';
+import 'package:firstapp/pages/second_page.dart';
 
 class FirstPage extends StatelessWidget {
   const FirstPage({super.key});
